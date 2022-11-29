b949e2aa0d1cc6ed11a65060226cc02871218a2d
mikulinskiy <32305540+andrewm8888@users.noreply.github.com>
2022-11-29 23:28:42 +0300

    commit 2 home work+

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b > a)
+{
+    Console.Write("Ответ: max ");
+    Console.Write(b);
+}
+if (a > b)
+{
+    Console.Write("Ответ: max ");
+    Console.Write(a);
+}
---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+if (b > a || b > c)
+{
+    Console.Write("Ответ: max ");
+    Console.Write(b);
+}
+if (a > b || a > c)
+{
+    Console.Write("Ответ: max ");
+    Console.Write(a);
+}
+if (c > b || c > a)
+{
+    Console.Write("Ответ: max ");
+    Console.Write(c);
+}
